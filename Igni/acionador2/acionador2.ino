c9bb12c3c639db98708f393f1956bd9c9a57b7dc
SteffanyOuverney <steffayouverney@gmail.com>
2022-10-17 16:41:59 -0300

    teste

--- Igni/acionador2/acionador2.ino
+++ Igni/acionador2/acionador2.ino
@@ -7,6 +7,7 @@ unsigned long button_press_time;
 unsigned long waiting_time = 10000;
 
 int playing = 0;
+
 void tone(byte pin, int freq) {
   ledcSetup(0, 2000, 8); // setup beeper
   ledcAttachPin(pin, 0); // attach beeper
