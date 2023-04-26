7931a9cf8aa342030bfd86e0dd6cca1e8d4407eb
SteffanyOuverney <steffayouverney@gmail.com>
2023-04-26 11:15:26 -0300

    Finalmente

---
+++ Celula de carga/Celula_de_carga_InfluxDB/Celula_de_carga_InfluxDB.ino
@@ -0,0 +1,109 @@
+/**
+ * Basic Write Example code for InfluxDBClient library for Arduino
+ * Data can be immediately seen in a InfluxDB UI: wifi_status measurement
+ * Enter WiFi and InfluxDB parameters below
+ *
+ * Measures signal level of the actually connected WiFi network
+ * This example supports only InfluxDB running from unsecure (http://...)
+ * For secure (https://...) or Influx Cloud 2 use SecureWrite example
+ **/
+
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
+#endif
+
+#include <InfluxDbClient.h>
+
+// WiFi AP SSID
+#define WIFI_SSID "LaRA"
+// WiFi password
+#define WIFI_PASSWORD "Iamarobot"
+// InfluxDB  server url. Don't use localhost, always server name or ip address.
+// E.g. http://192.168.1.48:8086 (In InfluxDB 2 UI -> Load Data -> Client Libraries), 
+#define INFLUXDB_URL "http://192.168.13.232:8086/"
+// InfluxDB 2 server or cloud API authentication token (Use: InfluxDB UI -> Load Data -> Tokens -> <select token>)
+#define INFLUXDB_TOKEN "cOXurPQTGuuF9jq0LhrWeInHP5E1YXHcNeX7ENAi9LdFx6KKuf5MFERVGGzF5pGsAealr5ee4fDvHXcaiPah6w=="
+// InfluxDB 2 organization id (Use: InfluxDB UI -> Settings -> Profile -> <name under tile> )
+#define INFLUXDB_ORG "UERJ"
+// InfluxDB 2 bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
+#define INFLUXDB_BUCKET "random"
+// InfluxDB v1 database name 
+//#define INFLUXDB_DB_NAME "database"
+
+// InfluxDB client instance
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+// InfluxDB client instance for InfluxDB 1
+//InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+
+// Data point
+Point sensor("wifi_status");
+
+#include "HX711.h"
+
+#define CELULA_DT  15
+#define CELULA_SCK  2
+
+HX711 escala;
+
+float fator_calib = -10000; // Coloque aqui o valor encontrado na calibração
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect WiFi
+  Serial.println("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  // Set InfluxDB 1 authentication params
+  //client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD);
+
+  // Add constant tags - only once
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  
+  escala.begin(CELULA_DT, CELULA_SCK);
+  escala.set_scale(fator_calib); // Ajusta a escala
+  escala.tare(); // Ajusta o zero da escala
+}
+
+void loop() {
+  // Store measured value into point
+  sensor.clearFields();
+  // Report RSSI of currently connected network
+  sensor.addField("test_escala", escala.get_units(1));
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+}
